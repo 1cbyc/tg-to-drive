8e4b1139306d7088e4bd96f038eac8b28614a8ac
1cbyc <isaacnsisong@gmail.com>
2025-12-05 12:36:53 -0800

    feat(core): Add __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,20 @@
+"""
+Core modules for Telegram to Google Drive Mirror
+"""
+
+from .config import Config
+from .downloader import TelegramDownloader
+from .uploader import DriveUploader
+from .processor import MirrorProcessor
+from .utils import format_size, has_media, get_file_info
+
+__all__ = [
+    'Config',
+    'TelegramDownloader',
+    'DriveUploader',
+    'MirrorProcessor',
+    'format_size',
+    'has_media',
+    'get_file_info',
+]
+
